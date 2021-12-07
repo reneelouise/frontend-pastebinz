ed0259fed9a12e24d32d606b2f5a4d80a7f73e03
Renee-Louise Nzegbulem <renee@Renee-Louises-MacBook-Pro.local>
2021-12-07 17:01:23 +0000

    paste and show react components created

--- src/components/PasteQuotes.tsx
+++ src/components/PasteQuotes.tsx
@@ -1,1 +1,19 @@
 import React from "react";
+import ShowQuotes from "./ShowQuotes";
+
+const PasteQuotes = () => {
+    return (
+        <>
+        <h1>RBG PASTEBIN 👸</h1>
+        <form>
+            <input className="add-quote"></input>
+            
+            </form>
+
+            </>
+    );
+    
+};
+
+
+export default PasteQuotes;
--- src/components/ShowQuotes.tsx
+++ src/components/ShowQuotes.tsx
@@ -1,1 +1,48 @@
+// import React, { useState } from "react";
 import React from "react";
+
+
+// interface Quotes {
+//     id: number,
+//     input: string
+// }
+
+
+// const ShowQuotes = async () => {
+//     const [result, setResult] = useState<Quotes[]>([])
+//     try {
+//         const response = await fetch("https://rbgdatabase.herokuapp.com/rbgquotes");
+//         const jsonData = await response.json();
+//         setResult(jsonData);
+//     }
+//     catch (err: any) {
+//         console.error(err.message)
+
+//     }
+
+const ShowQuotes = () =>{
+
+    return (
+        <>
+        
+        <div className = "container"> 
+        <table><tr>
+            <td>Public Pastes</td></tr>
+            </table>
+{/*         
+                {result.map((quote) => { 
+                <div key={quote.id}>
+                    <p>{quote.input}</p>
+                </div>
+            }
+
+            )} */}
+            
+            </div>
+        </>
+    );
+    
+};
+
+
+export default ShowQuotes;
