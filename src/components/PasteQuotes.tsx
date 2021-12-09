2d5864c5f6d8da01c3cff97e289e8c58c6d06b13
BeriKotelko <berenika@kotelko.com>
2021-12-09 11:53:10 +0000

    input field and button skeleton done, skeleton of handle function done

--- src/components/PasteQuotes.tsx
+++ src/components/PasteQuotes.tsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ShowQuotes from "./ShowQuotes";
 
 const PasteQuotes = () => {
-    return (
-        <>
-        <h1>RBG PASTEBIN 👸</h1>
-        
-            <textarea className="add-quote"></textarea>
-            <br></br>
-           
+  const [inputQuote, setInputQuote] = useState("");
+  return (
+    <>
+      <h1>RBG PASTEBIN 👸</h1>
 
-            </>
-    );
-    
+      <input
+        className="add-quote"
+        placeholder="add quote here"
+        value={inputQuote}
+        onChange={(e) => setInputQuote(e.target.value)}
+      ></input>
+      <br></br>
+      <button className="submitButton" onClick={() => handleSubmitQuote(inputQuote)}>Subimt</button>
+    </>
+  );
 };
 
-
+async function handleSubmitQuote(input: string | null) {
+try {
+    
+} catch (err:  any) {
+    console.error(err.message)
+    
+}
+}
 export default PasteQuotes;
